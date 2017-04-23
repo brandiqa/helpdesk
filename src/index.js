3b257184892f43a7b08b7ee8f9fddd3bf7c89181
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-23 12:42:09 +0300

    defined basic routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Route component={App} to="/" />
+  </BrowserRouter>,
   document.getElementById('root')
 );
