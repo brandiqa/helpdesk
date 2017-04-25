fe91421e2e658d10ebbae53607d1b2dca5376194
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-25 14:13:54 +0300

    Added drop down links

--- src/components/ticket-list.js
+++ src/components/ticket-list.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
-import { Menu, Segment, Icon }  from 'semantic-ui-react';
+import { Menu, Segment, Icon, Dropdown }  from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
+
+const options = [
+  {key:'all',value:'*',text:'All Tickets'},
+  {key:'owner',value:'owner',text:'My Tickets'},
+  {key:'open',value:'open',text:'Open Tickets'},
+  {key:'closed',value:'closed',text:'Closed Tickets'},
+]
 
 class TicketList extends Component {
   render() {
@@ -10,6 +17,9 @@ class TicketList extends Component {
           <NavLink className='item' to='/tickets'>
             <Icon name='chevron left'/>
           </NavLink>
+          <Menu.Item>
+            <Dropdown options={options} defaultValue='*'/>
+          </Menu.Item>
         </Menu>
         <Segment secondary style={{height:'95vh', marginTop:0}}>
           <p>List of Ticket Cards</p>
