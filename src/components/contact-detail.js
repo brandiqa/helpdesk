8bb5d5a89377274ad55edcb1aedcaf9a954033da
Michael Wanyoike <micwanyoike@gmail.com>
2017-04-26 14:31:17 +0300

    defined contact-detail

--- src/components/contact-detail.js
+++ src/components/contact-detail.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import { Item, Icon, Label } from 'semantic-ui-react';
 
 class ContactDetail extends Component {
   render() {
     return (
-      <p>contact info</p>
+      <Item.Group divided>
+        <Item>
+          <Label.Group circular style={{marginRight:'20px'}} size='huge'>
+            <Label content='JD'basic />
+          </Label.Group>
+          <Item.Content>
+            <Item.Header>
+              Jane Doe
+            </Item.Header>
+            <Item.Meta>
+              janedoe@example.com
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Icon name='user' circular size='large' style={{marginRight:'30px'}}/>
+          <Item.Content>
+            <Item.Meta>
+              Assigned To
+            </Item.Meta>
+            <Item.Header as='h5'>
+              Agent Smith
+            </Item.Header>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Icon name='line graph' circular size='large' style={{marginRight:'30px'}}/>
+          <Item.Content>
+            <Item.Meta>
+              Status
+            </Item.Meta>
+            <Item.Header as='h5'>
+              Open
+            </Item.Header>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Icon name='clock' circular size='large' style={{marginRight:'30px'}}/>
+          <Item.Content>
+            <Item.Meta>
+              Priority
+            </Item.Meta>
+            <Item.Header as='h5'>
+              High
+            </Item.Header>
+          </Item.Content>
+        </Item>
+        <Item>
+          <Icon name='info' circular size='large' style={{marginRight:'30px'}}/>
+          <Item.Content>
+            <Item.Meta>
+              Classification
+            </Item.Meta>
+            <Item.Header as='h5'>
+              Problem
+            </Item.Header>
+          </Item.Content>
+        </Item>
+      </Item.Group>
     )
   }
 }
